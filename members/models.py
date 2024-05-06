0347153da4d0855cf5c390daf2c41d6d122e6fe7
Magloire <debabel1hackamani@gmail.com>
2024-05-06 12:33:33 +0000

    add allMember and details tamplate; config views, models and urls

--- members/models.py
+++ members/models.py
@@ -3,4 +3,9 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phones = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
+
+
+
      
--- members/views.py
+++ members/views.py
@@ -1,8 +1,38 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
+
 
 def members(request):
-    template = loader.get_template("myfirst.html")
-    return HttpResponse(template.render())
+    # Create a members object with all values of the Member model
+    mymembers = Member.objects.all().values()
+    # Loads the all_members template
+    template = loader.get_template("all_members.html")
+    
+    # Creates an object containing the mymembers object.
+    context = {
+        "mymembers": mymembers
+    }
+    # Sends the object to the template.
+    # Outputs the HTML that is rendered by the template.
+    return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    # Gets the id as an argument.
+    # Uses the id to locate the correct record in the Member table.
+    mymember = Member.objects.get(id=id)
+
+    # Create an object containing the mymember object
+    context = {
+        "mymember": mymember
+    }
+    
+    # loads the details.html template.
+    # Sends the object to the template.
+    # Outputs the HTML that is rendered by the template.
+    template = loader.get_template("details.html")
+
+    return HttpResponse(template.render(context, request))
+    
 
