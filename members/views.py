134ca2004bd1e7957f5a3c883269213ef1a3f32e
Magloire <debabel1hackamani@gmail.com>
2024-05-09 10:01:13 +0000

    add superuser in admin

--- members/views.py
+++ members/views.py
@@ -41,3 +41,10 @@ def main(request):
     return HttpResponse(template.render())
 
 
+# Add testing view
+def testing(request):
+    template = loader.get_template("template.html")
+    context = {
+        "fruits":["Apple", "Banana", "Cherry"],
+    }
+    return HttpResponse(template.render(context, request))
