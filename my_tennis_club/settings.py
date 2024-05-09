fe7d6dad424454f410eae7bfd3bf28ac1a88a4b0
Magloire <debabel1hackamani@gmail.com>
2024-05-09 00:49:14 +0000

    add 404 page and modify setting

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r^^6j$+_tn_7tp-_4&!5=zjoh^40qc=vh&aj6p15(0n&5h455m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
